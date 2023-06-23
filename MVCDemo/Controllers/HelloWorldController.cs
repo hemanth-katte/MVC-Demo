66412f28e7688f5a8f1bc9166dd3fd0c130513a0
Hemanth Katte <hemanthss24@gmail.com>
2023-06-23 07:21:05 +0530

    Add project files.

---
+++ MVCDemo/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {   
+
+            return View();
+        }
+
+        public IActionResult HelloWorld()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ MVCDemo/Models/DogViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCDemo.Models
+{
+	public class DogViewModel
+	{
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
